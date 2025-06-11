965117d31a46369caf3a5d6da25f52c11b8fb348
Prajyot Korde <kordeprajyot@gmail.com>
2025-06-11 20:45:31 +0530

    Navbar_done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -27,9 +27,13 @@ const Navbar = () => {
         <img src={search_icon} alt="" className='icon' />
         <p>Children</p>
         <img src={bell_icon} alt="" className='icon' />
-          <div className="navbar_profile">
+          <div className="navbar-profile">
             <img src={profile_img} alt=""  className='profile'/>
             <img src={caret_icon} alt="" />
+
+            <div className='dropdown'>
+              <p>Sign Out Of Netflix</p>
+            </div>
           </div>
       </div>
     </div>
